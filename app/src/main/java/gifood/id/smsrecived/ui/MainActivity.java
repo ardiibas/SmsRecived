7306093e42d54ca865c0b611d2f72669974420f1
ardiibas <mistersemar.ab@gmail.com>
2018-05-23 09:49:33 +0700

    add BroadcastReceiver

--- app/src/main/java/gifood/id/smsrecived/ui/MainActivity.java
+++ app/src/main/java/gifood/id/smsrecived/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package gifood.id.smsrecived;
+package gifood.id.smsrecived.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import gifood.id.smsrecived.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/gifood/id/smsrecived/ui/SmsReceiverActivity.java
+++ app/src/main/java/gifood/id/smsrecived/ui/SmsReceiverActivity.java
@@ -1,10 +1,12 @@
-package gifood.id.smsrecived;
+package gifood.id.smsrecived.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import gifood.id.smsrecived.R;
 
 public class SmsReceiverActivity extends AppCompatActivity implements View.OnClickListener {
 
